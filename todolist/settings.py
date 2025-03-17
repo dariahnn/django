68cae840237c5a2d7eac2a9fc39064918c1a3cc3
dariahnn <dariananandaz@gmail.com>
2025-03-17 16:41:40 +0300

    second commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolistapp'
+    'todolistapp',
+    'todolistappappi',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
---
+++ todolistappappi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodolistappappiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todolistappappi'
